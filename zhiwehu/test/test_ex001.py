2833062d82fa1e4f37c1deacb12deccd16206bf4
Carl Hinderer <carl.hinderer4@gmail.com>
2019-03-25 15:01:50 -0600

    Finished exercise 1 tests.

--- zhiwehu/test/test_ex001.py
+++ zhiwehu/test/test_ex001.py
@@ -3,4 +3,14 @@ import pytest
 
 def test_div_7_not_5():
 	numbers = div_7_not_5()
-	assert True
+	first_value = numbers[0]
+	last_value = numbers[-1]
+
+	assert first_value >= 2000
+	assert first_value % 7 == 0
+	
+	assert last_value <= 3200
+	assert last_value % 7 == 0
+
+	for i in range(10): 
+		assert numbers[i] % 5 != 0
