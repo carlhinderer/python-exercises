9d35ab3376570a13a576335cb30b542a55129ddc
Carl Hinderer <carl.hinderer4@gmail.com>
2019-09-25 12:35:57 -0600

    Added daily problems 46-49.

---
+++ daily-coding-problems/problem008.py
@@ -0,0 +1,20 @@
+# Problem 8
+#
+# A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+#   Note that a leaf counts as a unival tree.
+#
+# Given the root to a binary tree, count the number of unival subtrees.
+#
+# For example, the following tree has 5 unival subtrees:
+#
+#   0
+#  / \
+# 1   0
+#    / \
+#   1   0
+#  / \
+# 1   1
+
+def count_unival_subtrees(root):
+
+def 
