fb433ce0dbde7c1f95123916ce269db96d5b021a
Carl Hinderer <carl.hinderer4@gmail.com>
2019-10-27 12:40:19 -0600

    Added daily exercises 79-81.

---
+++ daily-coding-problems/problem080.py
@@ -0,0 +1,13 @@
+# Problem 80
+#   Easy
+#   Asked by Google
+#
+# Given the root of a binary tree, return a deepest node. For example, in the 
+#   following tree, return d.
+#
+#     a
+#    / \
+#   b   c
+#  /
+# d
+#
