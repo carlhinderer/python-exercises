0201954299dcb37d9dda31e4db941c9f6894a620
Carl Hinderer <carl.hinderer4@gmail.com>
2019-11-11 14:58:36 -0700

    Added daily exercises 91-96.

---
+++ daily-coding-problems/problem091.py
@@ -0,0 +1,14 @@
+# Exercise 91
+#   Easy
+#   Asked by Dropbox
+#
+# What does the below code snippet print out? How can we fix the anonymous functions 
+#   to behave as we'd expect?
+#
+#   functions = []
+#   for i in range(10):
+#       functions.append(lambda : i)
+#
+#   for f in functions:
+#       print(f())
+#
