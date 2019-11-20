d3571d08bc7429016d7cba9aef4a8cda99f40516
Carl Hinderer <carl.hinderer4@gmail.com>
2019-11-20 10:28:34 -0700

    Added daily exercises 101-105.

---
+++ daily-coding-problems/problem104.py
@@ -0,0 +1,8 @@
+# Problem 104
+#   Easy
+#   Asked by Google
+#
+# Determine whether a doubly linked list is a palindrome. What if it’s singly linked?
+#
+# For example, 1 -> 4 -> 3 -> 4 -> 1 returns True while 1 -> 4 returns False.
+#
