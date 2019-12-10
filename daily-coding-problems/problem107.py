60d6e1c2465d57698c188f4afc649df0ef3f62f3
Carl Hinderer <carl.hinderer4@gmail.com>
2019-12-09 22:29:57 -0700

    Added daily exercises 106-110.

---
+++ daily-coding-problems/problem107.py
@@ -0,0 +1,12 @@
+# Problem 107
+#   Easy
+#   Asked by Microsoft
+#
+# Print the nodes in a binary tree level-wise. For example, the following should print 1, 2, 3, 4, 5.
+#
+#     1
+#    / \
+#   2   3
+#      / \
+#     4   5
+#
