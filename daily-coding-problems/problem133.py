955689ee63342d243247f4dc9aa6cb5ee04f0b2b
Carl Hinderer <carl.hinderer4@gmail.com>
2020-01-03 20:07:20 -0700

    Added daily exercises 133-136.

---
+++ daily-coding-problems/problem133.py
@@ -0,0 +1,17 @@
+# Problem 133
+#   Medium
+#   Asked by Amazon
+#
+# Given a node in a binary search tree, return the next bigger element, also known as the inorder 
+#   successor.
+#
+# For example, the inorder successor of 22 is 30.
+#
+#    10
+#   /  \
+#  5    30
+#      /  \
+#    22    35
+#
+# You can assume each node has a parent pointer.
+#
