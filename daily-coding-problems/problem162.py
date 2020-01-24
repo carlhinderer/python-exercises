6acc2ca4f6f74105434c98a2a264f9dfc34e714a
Carl Hinderer <carl.hinderer4@gmail.com>
2020-01-23 21:33:23 -0700

    Added daily exercises 162-165.

---
+++ daily-coding-problems/problem162.py
@@ -0,0 +1,21 @@
+# Problem 162
+#   Medium
+#   Asked by Square
+#
+# Given a list of words, return the shortest unique prefix of each word. For 
+#   example, given the list:
+#
+#    dog
+#    cat
+#    apple
+#    apricot
+#    fish
+#
+# Return the list:
+#
+#    d
+#    c
+#    app
+#    apr
+#    f
+#
