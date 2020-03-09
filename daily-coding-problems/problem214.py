76ea0d17fdc1cf292d34d3e7ebf565ff6ff02049
Carl Hinderer <carl.hinderer4@gmail.com>
2020-03-09 13:46:08 -0600

    Added daily exercises 212-215.

---
+++ daily-coding-problems/problem214.py
@@ -0,0 +1,9 @@
+# Problem 214
+#   Easy
+#   Asked by Stripe
+#
+# Given an integer n, return the length of the longest consecutive run of 1s in 
+#   its binary representation.
+#
+# For example, given 156, you should return 3.
+#
