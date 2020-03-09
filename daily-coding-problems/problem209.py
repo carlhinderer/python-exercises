fbabd639f480443ce6cb25c5e7a009afce79c5b1
Carl Hinderer <carl.hinderer4@gmail.com>
2020-03-09 13:41:40 -0600

    Added daily exercises 208-211.

---
+++ daily-coding-problems/problem209.py
@@ -0,0 +1,9 @@
+# Problem 209
+#   Hard
+#   Asked by YouTube
+#
+# Write a program that computes the length of the longest common subsequence of 
+#   three given strings. For example, given "epidemiologist", "refrigeration", and 
+#   "supercalifragilisticexpialodocious", it should return 5, since the longest common 
+#   subsequence is "eieio".
+#
---
+++ daily-coding-problems/problem211.py
@@ -0,0 +1,8 @@
+# Problem 211
+#   Medium
+#   Asked by Microsoft
+#
+# Given a string and a pattern, find the starting indices of all occurrences of the 
+#   pattern in the string. For example, given the string "abracadabra" and the 
+#   pattern "abr", you should return [0, 7].
+#
