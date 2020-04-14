cec3d7ec20017060fc84efbcf69e84126d261d31
Carl Hinderer <carl.hinderer4@gmail.com>
2020-04-14 11:58:54 -0600

    Added daily exercises 248-251.

---
+++ daily-coding-problems/problem248.py
@@ -0,0 +1,7 @@
+# Problem 248
+#   Hard
+#   Asked by Nvidia
+#
+# Find the maximum of two numbers without using any if-else statements, branching, 
+#   or direct comparisons.
+#
---
+++ daily-coding-problems/problem249.py
@@ -0,0 +1,6 @@
+# Problem 249
+#   Hard
+#   Asked by SalesForce
+#
+# Given an array of integers, find the maximum XOR of any two elements.
+#
