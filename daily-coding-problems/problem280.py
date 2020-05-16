475a61e0033e6af2a62278c43bb20e01f6c6be33
Carl Hinderer <carl.hinderer4@gmail.com>
2020-05-16 10:14:34 -0600

    Added daily exercises 280-283.

---
+++ daily-coding-problems/problem280.py
@@ -0,0 +1,6 @@
+# Problem 280
+#   Easy
+#   Asked by Pandora
+#
+# Given an undirected graph, determine if it contains a cycle.
+#
---
+++ daily-coding-problems/problem282.py
@@ -0,0 +1,7 @@
+# Problem 282
+#   Easy
+#   Asked by Netflix
+#
+# Given an array of integers, determine whether it contains a Pythagorean triplet. Recall 
+#   that a Pythogorean triplet (a, b, c) is defined by the equation a**2 + b**2 = c**2.
+#
