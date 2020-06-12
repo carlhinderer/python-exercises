ce0043ee942fe5f2fa9914a731c645d3517ce9c3
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-12 13:59:46 -0600

    Added daily exercises 301-305.

---
+++ daily-coding-problems/problem303.py
@@ -0,0 +1,9 @@
+# Problem 303
+#   Easy
+#   Asked by Microsoft
+#
+# Given a clock time in hh:mm format, determine, to the nearest degree, the angle between 
+#   the hour and the minute hands.
+#
+# Bonus: When, during the course of a day, will the angle be zero?
+#
