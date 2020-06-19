a4863ec9d65fd535908af0555e11c3fb7bb7fd31
Carl Hinderer <carl.hinderer4@gmail.com>
2020-06-19 14:17:42 -0600

    Did exercise 317.

--- daily-coding-problems/problem317.py
+++ daily-coding-problems/problem317.py
@@ -5,3 +5,14 @@
 # Write a function that returns the bitwise AND of all integers between M and N, 
 #   inclusive.
 #
+
+import functools
+
+def bitwise_and_for_range(m, n):
+    numbers = range(m, n+1)
+    return functools.reduce(lambda a, b: a & b, numbers)
+
+
+if __name__ == '__main__':
+    result = bitwise_and_for_range(50, 55)
+    print('Result: ', result)
