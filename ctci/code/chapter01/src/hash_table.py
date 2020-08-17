cbf23ec71739993554fb84657e31127dcee15448
Carl Hinderer <carl.hinderer4@gmail.com>
2020-08-17 14:41:58 -0600

    Added sample hash table implementation.

---
+++ ctci/code/chapter01/src/hash_table.py
@@ -0,0 +1,59 @@
+# This code taken from @marcus here
+#   https://coderbook.com/@marcus/how-to-create-a-hash-table-from-scratch-in-python/
+#
+
+class HashTable(object):
+
+    def __init__(self, length=4):
+        self.array = [None] * length
+
+    def set(self, key, value):
+        index = self.hash(key)
+        if self.array[index] is None:
+            self.array[index] = []
+            self.array[index].append([key, value])
+        else:
+            for kvp in self.array[index]:
+                if kvp[0] == key:
+                    kvp[1] = value
+                    break
+            else:
+                self.array[index].append([key, value])
+        if self.is_full():
+            self.double()
+
+    def get(self, key):
+        index = self.hash(key)
+        if self.array[index] is None:
+            raise KeyError()
+        else:
+            for kvp in self.array[index]:
+                if kvp[0] == key:
+                    return kvp[1]
+            raise KeyError()
+
+    def hash(self, key):
+        length = len(self.array)
+        return hash(key) % length
+
+    def is_full(self):
+        items = 0
+        for item in self.array:
+            if item is not None:
+                items += 1
+        return items > len(self.array)/2
+        
+    def double(self):
+        ht2 = HashTable(length=len(self.array)*2)
+        for i in range(len(self.array)):
+            if self.array[i] is None:
+                continue
+            for kvp in self.array[i]:
+                ht2.add(kvp[0], kvp[1])
+        self.array = ht2.array
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+    
+    def __getitem__(self, key):
+        return self.get(key)
---
+++ ctci/code/chapter01/test/test_hash_table.py
@@ -0,0 +1,21 @@
+import pytest
+from ..src.hash_table import *
+
+def test_setitem():
+    ht = HashTable()
+
+    ht['foo'] = 'bar'
+    assert(ht['foo'] == 'bar')
+
+    ht['foo'] = 'baz'
+    assert(ht['foo'] == 'baz')
+
+
+def test_getitem():
+    ht = HashTable()
+    ht['foo'] = 'bar'
+
+    assert(ht['foo'] == 'bar')
+
+    with pytest.raises(KeyError):
+        ht['baz']
