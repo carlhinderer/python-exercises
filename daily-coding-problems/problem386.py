677561cc073b6400fbd017cd4eed7f8af33a6de8
Carl Hinderer <carl.hinderer4@gmail.com>
2020-08-28 11:02:43 -0600

    Added daily exercises 383-386.

---
+++ daily-coding-problems/problem386.py
@@ -0,0 +1,9 @@
+# Problem 386
+#   Easy
+#   Asked by Twitter
+#
+# Given a string, sort it in decreasing order based on the frequency of characters. If 
+#   there are multiple possible solutions, return any of them.
+#
+# For example, given the string tweet, return tteew. eettw would also be acceptable.
+#
