2d227baec37b042ca3af9f628954e2c325301685
Carl Hinderer <carl.hinderer4@gmail.com>
2020-09-06 12:37:08 -0600

    Added daily exercises 391-395.

---
+++ daily-coding-problems/problem395.py
@@ -0,0 +1,16 @@
+# Problem 395
+#   Medium
+#   Asked by Robinhood
+#
+# Given an array of strings, group anagrams together.
+#
+# For example, given the following array:
+#
+#   ['eat', 'ate', 'apt', 'pat', 'tea', 'now']
+#
+# Return:
+#
+#   [['eat', 'ate', 'tea'],
+#    ['apt', 'pat'],
+#    ['now']]
+#
