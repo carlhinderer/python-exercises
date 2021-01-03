f6b6df25e5a6f0ed600bf795c45ac7e6d0b2fe98
Carl Hinderer <carl.hinderer4@gmail.com>
2021-01-03 11:12:11 -0700

    Added daily exercises 504-508.

---
+++ daily-coding-problems/problem505.py
@@ -0,0 +1,7 @@
+# Problem 505
+#   Easy
+#   Asked by Amazon
+#
+# Given an array and a number k that's smaller than the length of the array, rotate 
+#   the array to the right k elements in-place.
+#
