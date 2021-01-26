fa11f233c6f9075de719bdd78365d460ed9b87ac
Carl Hinderer <carl.hinderer4@gmail.com>
2021-01-26 10:19:51 -0700

    Added daily exercises 531-535.

---
+++ daily-coding-problems/problem531.py
@@ -0,0 +1,10 @@
+# Problem 531
+#   Easy
+#   Asked by Microsoft
+#
+# Using a read7() method that returns 7 characters from a file, implement readN(n) which reads 
+#   n characters.
+#
+# For example, given a file with the content “Hello world”, three read7() returns “Hello w”, “orld” 
+#   and then “”.
+#
