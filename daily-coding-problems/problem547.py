4e09bbb4c4e2bd5644ed50e997db9f3c289a18f7
Carl Hinderer <carl.hinderer4@gmail.com>
2021-02-09 11:21:01 -0700

    Added daily exercises 546-549.

---
+++ daily-coding-problems/problem547.py
@@ -0,0 +1,6 @@
+# Problem 547
+#   Hard
+#   Asked by Salesforce
+#
+# Given an array of integers, find the maximum XOR of any two elements.
+#
---
+++ daily-coding-problems/problem548.py
@@ -0,0 +1,9 @@
+# Problem 548
+#   Easy
+#   Asked by Microsoft
+#
+# Given a clock time in hh:mm format, determine, to the nearest degree, the angle between the 
+#   hour and the minute hands.
+#
+# Bonus: When, during the course of a day, will the angle be zero?
+#
